d19689d1d51549f5a6b24af52d26d7dc487c3e8c
Krystian Fras <krystian@fras.me>
2024-09-28 22:23:05 +0200

    Change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['monospace'],
+      },
+    },
     container: {
       center: true,
       padding: "2rem",
